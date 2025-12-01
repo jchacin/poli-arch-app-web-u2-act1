4c968050bc45a977f68fb24edf8d5082e4ea8b9b
José <josegil097@gmail.com>
2025-12-01 14:42:48 -0500

    Activar Swagger

--- ProductAPI/Program.cs
+++ ProductAPI/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi;
 using ProductAPI.Business;
 using ProductAPI.Business.Interfaces;
 using ProductAPI.Middlewares;
@@ -9,8 +10,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Swagger / OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ProductAPI",
+        Version = "v1",
+        Description = "API para gestionar productos"
+    });
+});
+
 builder.Services.AddTransient<IProductService, ProductService>();
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
 
@@ -23,7 +35,13 @@ app.UseErrorHandler();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProductAPI v1");
+        c.RoutePrefix = "swagger"; // opcional: /swagger
+    });
 }
 
 app.UseHttpsRedirection();
